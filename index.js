b712564ddac500005666b7ad4ac567de5f69fc0d
张庭岑 <ztc01@baidu.com>
2018-09-03 20:11:30 +0800

    feat: allow empty scope

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = {
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 72],
     'scope-case': [0, 'always', 'lower-case'],
+    'scope-empty': [0, 'always'],
     'subject-case': [0, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
